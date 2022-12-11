4bf90c3fdc200fa1d8325e6c5477806c7488f8b1
kirill_off <kirilloffkostya@gmail.com>
2022-12-11 19:26:56 +0300

    fix after review

--- MovieQuizUITests/MovieQuizUITests.swift
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -13,34 +13,25 @@ class MovieQuizUITests: XCTestCase {
 
     override func setUpWithError() throws {
         try super.setUpWithError()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
         app = XCUIApplication()
         app.launch()
         
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
         try super.tearDownWithError()
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         app.terminate()
         app = nil
     }
 
     func testExample() throws {
-        // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
             measure(metrics: [XCTApplicationLaunchMetric()]) {
                 XCUIApplication().launch()
             }
@@ -72,7 +63,6 @@ class MovieQuizUITests: XCTestCase {
         let indexText = app.staticTexts["Index"].label
         XCTAssertNotEqual(firstPoster, secondPoster)
         XCTAssertTrue(indexText == "2/10")
-        
     }
     
     func testShowResultAlert() throws {
@@ -87,7 +77,6 @@ class MovieQuizUITests: XCTestCase {
         XCTAssertTrue(alert.exists)
         XCTAssertTrue(alert.buttons.firstMatch.label == "Сыграть еще раз")
         XCTAssertTrue(alert.label == "Раунд окончен!")
-        
     }
     
     func testAlertDismiss() throws {
